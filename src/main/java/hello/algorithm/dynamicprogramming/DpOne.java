90b6feae84641838051d0dc6308d2254238f2305
ksungsu <zxzxzlxlzlxl@naver.com>
2022-06-22 12:45:52 +0900

    dynamic programming 백준

--- src/main/java/hello/algorithm/dynamicprogramming/DpOne.java
+++ src/main/java/hello/algorithm/dynamicprogramming/DpOne.java
@@ -10,30 +10,32 @@ public class DpOne {
 
         int N = Integer.parseInt(br.readLine());
 
-        dp = new Integer[N + 1];
-        dp[0] = dp[1] = 0;
 
-        System.out.print(recur(N));
+        dp = new Integer[N+1]; //dp라는 배열에 N+1만큼의 공간이 마련되었다. 예를들어 N을 5라고 입력했을 시에 dp는 6의 공간이 마련된다.
+        //6의 공간은 dp[0]~dp[6]으로 해석할 수 있다.
+        dp[0]=dp[1]=0;
+
+        System.out.println(rec(N));
 
 
     }
 
-
-    static int recur(int N) {
-        if (dp[N] == null) {
-            if (N % 6 == 0) {
-                dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
-            } else if (N % 3 == 0) {
-                dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
-            } else if (N % 2 == 0) {
-                dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
-            } else {
-                dp[N] = recur(N - 1) + 1;
+    static int rec(int N){
+        if(dp[N]==null){
+            if(N % 6 == 0){
+                dp[N]=Math.min(rec(N-1), Math.min(rec(N /3), rec(N/2)))+1;
             }
-
+            else if(N % 3 == 0){
+                dp[N]=Math.min(rec(N /3), rec(N-1))+1;
+            }
+            else if(N % 2 ==0){
+                dp[N]=Math.min(rec(N /2 ), rec(N - 1))+1;
+            }else{
+                dp[N]=rec(N-1)+1;
+            }
         }
         return dp[N];
     }
-    }
+}
 
 
