41e3f62daf31a788b5fbf59ff49d1edb48cf5043
ksungsu <zxzxzlxlzlxl@naver.com>
2022-06-30 17:03:26 +0900

    dynamic programming 백준

--- src/main/java/hello/algorithm/dynamicprogramming/DpNine.java
+++ src/main/java/hello/algorithm/dynamicprogramming/DpNine.java
@@ -1,21 +1,41 @@
 package hello.algorithm.dynamicprogramming;
 import java.io.*;
+/*
+백준(2156)/ 포도주 시식
+ */
 
 public class DpNine {
-    static int[] dp;
-    final static int mod = 10007;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        dp = new int[n+1];
-        dp[0] =0;
-        dp[1] = 1;
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-2]+dp[i-1];
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for(int i = 0; i < n; i++){
+                arr[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(dp[n]);
+
+
+        //첫잔일 경우
+        if(n >= 1){
+            dp[0] = arr[0];
+        }
+
+        //두잔일 경우
+        if(n >= 2){
+            dp[1] = arr[0] + arr[1];
+        }
+
+        //세잔일 경우
+        if(n >= 3){
+            dp[2] = Math.max(dp[1], Math.max(dp[0] + arr[2], arr[1]+arr[2]));
+        }
+        for(int i = 3; i < n; i++){
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]));
+        }
+        System.out.println(dp[n-1]);
     }
 }
 /*
