9ae79613f0160ee08ff1fc181038cc47c52517f7
ksungsu <zxzxzlxlzlxl@naver.com>
2022-07-07 20:43:15 +0900

    dynamic programming 백준

--- src/main/java/hello/algorithm/dynamicprogramming/DpTen.java
+++ src/main/java/hello/algorithm/dynamicprogramming/DpTen.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 /*
-가장 긴 증가하는 부분 수열
+가장 긴 증가하는 부분 수열 백준 11053
  */
 
 public class DpTen {
@@ -30,6 +30,8 @@ public class DpTen {
             for(int j = 0; j < i; j++){
                 if(arr[j] < arr[i] && dp[i] <= dp[j]){
                     dp[i] = dp[j] + 1;
+                }else if(arr[i] == arr[j]){
+                    dp[i] = dp[j];
                 }
             }
         }
@@ -40,7 +42,7 @@ public class DpTen {
             max = Math.max(max, i);
         }
 
-        System.out.println(dp[n-1]);
+        System.out.println(max);
             }
         }
 
