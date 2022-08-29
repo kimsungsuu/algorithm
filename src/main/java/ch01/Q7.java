3b67b679b67b6359404c6c54d0d56f0ca28da663
ksungsu <zxzxzlxlzlxl@naver.com>
2022-08-30 01:01:13 +0900

    dynamic programming 백준

---
+++ src/main/java/ch01/Q7.java
@@ -0,0 +1,27 @@
+package ch01;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        int sum = 0;
+        int i;
+
+        int n = stdIn.nextInt();
+
+        for(i = 1; i <= n; i++){
+
+            sum += i;
+
+            if(i < n) {
+                System.out.print(i + " + ");
+            }else{
+                System.out.print(i + " = " + sum);
+            }
+
+        }
+
+    }
+}
