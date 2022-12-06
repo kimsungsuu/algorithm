ea527543090ed64df8fbf34100aea1d80bf1aacd
ksungsu <zxzxzlxlzlxl@naver.com>
2022-12-07 00:14:48 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/package02/eight/Main.java
+++ src/main/java/infrenalgorithm/package02/eight/Main.java
@@ -6,44 +6,13 @@ public class Main {
 
     public int[] solution(int n, int[] arr) {
         int[] answer = new int[n];
-        int cnt;
 
-        for(int i = 0; i < n-1; i++){
-            cnt = 1;
-            for(int j = i+1; j < n; j++){
-                if(arr[i] < arr[j]){
-                    cnt++;
-                    answer[i] = cnt;
-                }else{
-                    answer[i] = cnt;
-                }
+        for(int i = 0; i < n; i++){
+            int cnt = 1;
+            for(int j = 0; j < n; j++){
+                if(arr[i] < arr[j]) cnt++;
             }
-
-            if(i >= 1){
-                for(int k = i-1; k >= 0; k--)
-                {
-                    if(arr[i] < arr[k]){
-                        cnt++;
-                        answer[i] = cnt;
-                    }
-                    else{
-                        answer[i] = cnt;
-                    }
-                }
-            }
-        }
-
-        cnt = 1;
-
-        for(int i = n-1; i>=n-1; i--){
-            for(int j = i-1; j >= 0; j--){
-                if(arr[i] < arr[j]){
-                    cnt++;
-                    answer[i] = cnt;
-                }else{
-                    answer[i] = cnt;
-                }
-            }
+            answer[i] = cnt;
         }
 
         return answer;
