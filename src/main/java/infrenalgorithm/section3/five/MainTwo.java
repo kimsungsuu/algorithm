b4e14db91d7e5d89277fa96a5f3d3aff75d5c012
ksungsu <zxzxzlxlzlxl@naver.com>
2022-12-13 19:32:00 +0900

    study java algorithm

---
+++ src/main/java/infrenalgorithm/section3/five/MainTwo.java
@@ -0,0 +1,31 @@
+package infrenalgorithm.section3.five;
+
+import java.util.Scanner;
+
+public class MainTwo {
+
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = 1;
+
+        n--;
+
+        while (n > 0) {
+            cnt++;
+            n = n - cnt;
+
+            if (n % cnt == 0) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MainTwo T = new MainTwo();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        System.out.println(T.solution(n));
+    }
+}
