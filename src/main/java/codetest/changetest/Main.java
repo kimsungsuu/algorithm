4e0d5b12fdb3910626a2a061ddcef116a4143bdb
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-04 14:18:54 +0900

    study java algorithm

--- src/main/java/codetest/changetest/Main.java
+++ src/main/java/codetest/changetest/Main.java
@@ -1,44 +1,17 @@
 package codetest.changetest;
 
 import java.util.Scanner;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
 
-    class Person{
-        int num;
-        int priority;
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        public Person(int num, int priority){
-            this.num = num;
-            this.priority = priority;
-        }
-    }
+        answer.add(arr[0]);
 
-    public int solution(int n, int m, int[] arr){
-        int answer = 1;
-
-        Queue<Person> q = new LinkedList<>();
-
-        for(int i = 0; i < n; i++){
-            q.offer(new Person(i, arr[i]));
-        }
-
-
-        while(!q.isEmpty()){
-            Person tmp = q.poll();
-            for(Person x : q){
-                if(x.priority > tmp.priority){
-                    q.offer(tmp);
-                    tmp = null;
-                    break;
-                }
-            }
-            if(tmp != null) {
-                if (tmp.num == m) return answer;
-                else answer++;
-            }
+        for(int i = 1; i < n; i++){
+            if(arr[i-1] < arr[i]) answer.add(arr[i]);
         }
 
         return answer;
@@ -49,13 +22,12 @@ public class Main {
         Scanner in=new Scanner(System.in);
 
         int n = in.nextInt();
-        int m = in.nextInt();
         int[] arr = new int[n];
 
         for(int i = 0; i < n; i++){
             arr[i] = in.nextInt();
         }
 
-        System.out.println(T.solution(n, m, arr));
+        System.out.println(T.solution(n, arr));
     }
 }
