f93cb8a3cfed0aec88018ca92a843d38b1298312
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-06 15:37:03 +0900

    study java algorithm

--- src/main/java/codetest/changetest/Main.java
+++ src/main/java/codetest/changetest/Main.java
@@ -3,18 +3,20 @@ package codetest.changetest;
 import java.util.Scanner;
 
 public class Main {
-
     public int solution(int n, int k, int[] arr){
         int answer = 0;
         int lt = 0, sum = 0;
 
         for(int rt = 0; rt < n; rt++){
             sum += arr[rt];
-            if(rt >= k){
+
+            if(sum == k) answer++;
+
+            while(sum > k){
                 sum -= arr[lt];
                 lt++;
+                if(sum == k) answer++;
             }
-            answer = Math.max(answer, sum);
         }
 
         return answer;
