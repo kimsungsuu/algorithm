f2c8c8547ebab9974c0d143607298cd41919a9db
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-06 15:59:08 +0900

    study java algorithm

--- src/main/java/codetest/changetest/Main.java
+++ src/main/java/codetest/changetest/Main.java
@@ -1,21 +1,22 @@
 package codetest.changetest;
 
+
 import java.util.Scanner;
 
 public class Main {
-    public int solution(int n, int k, int[] arr){
+    public int solution(int n){
         int answer = 0;
         int lt = 0, sum = 0;
 
-        for(int rt = 0; rt < n; rt++){
-            sum += arr[rt];
+        for(int rt = 0; rt <= n/2+1; rt++){
+            sum += rt;
 
-            if(sum == k) answer++;
+            if(sum == n) answer++;
 
-            while(sum > k){
-                sum -= arr[lt];
+            while(sum > n){
+                sum -= lt;
                 lt++;
-                if(sum == k) answer++;
+                if(sum == n) answer++;
             }
         }
 
@@ -27,14 +28,7 @@ public class Main {
         Scanner in=new Scanner(System.in);
 
         int n = in.nextInt();
-        int k = in.nextInt();
 
-        int[] arr = new int[n];
-
-        for(int i = 0; i < n; i++){
-            arr[i] = in.nextInt();
-        }
-
-        System.out.println(T.solution(n, k, arr));
+        System.out.println(T.solution(n));
     }
 }
