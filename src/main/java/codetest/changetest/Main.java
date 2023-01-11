1ca9ca9c075f22cace372821958986f38eeb821f
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-11 23:49:58 +0900

    study java algorithm

--- src/main/java/codetest/changetest/Main.java
+++ src/main/java/codetest/changetest/Main.java
@@ -21,11 +21,14 @@ public class Main {
 
                 map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
 
-                map.put(arr[lt], map.get(arr[lt])-1);
-                if(map.get(arr[lt])==0) map.remove(map.remove(arr[lt]));
-                lt++;
+            answer.add(map.size());
 
-                answer.add(map.size());
+            map.put(arr[lt], map.get(arr[lt])-1);
+
+            if(map.get(arr[lt])==0) map.remove(map.remove(arr[lt]));
+
+            lt++;
+
         }
 
         return answer;
