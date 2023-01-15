7272edeb162c83c8fe8baf03b87c36ccbe0417a2
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-15 14:59:47 +0900

    study java algorithm

--- src/main/java/codetest/changetest/Main.java
+++ src/main/java/codetest/changetest/Main.java
@@ -1,41 +1,28 @@
 package codetest.changetest;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.Collections;
 
 public class Main {
     public int solution(int n, int m, int[] arr){
 
-        int answer = 0;
+        int answer = -1;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        TreeSet<Integer> tSet = new TreeSet<>(Collections.reverseOrder());
+
 
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
                 for(int k = j+1; k < n; k++){
-                    int tmp = arr[i] + arr[j] + arr[k];
-                    map.put(tmp, map.getOrDefault(tmp, 0) + 1);
+                    tSet.add(arr[i] + arr[j] + arr[k]);
                 }
             }
         }
         int cnt = 0;
-
-        while(cnt != m && !map.isEmpty()){
-            int tmp = 0;
-            for(int x : map.keySet()){
-                tmp = Math.max(tmp, x);
-            }
-            if(tmp != 0){
-                System.out.println(map);
-                map.remove(tmp);
-                cnt++;
-            }
-            answer = tmp;
-        }
-
-        if(cnt < m){
-            answer = -1;
+        for(int x : tSet){
+            cnt++;
+            if(cnt == m) answer = x;
         }
 
         return answer;
