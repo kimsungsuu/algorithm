e39abac49b5854bfb79fd1cc78de7fa1b073e89c
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-21 23:24:19 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section06/five/Main.java
+++ src/main/java/infrenalgorithm/section06/five/Main.java
@@ -1,17 +1,31 @@
 package infrenalgorithm.section06.five;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public char solution(int n, int[] arr){
         char answer = 'U';
+/**
+ * 아래 코드는 해쉬맵 방법
+ */
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//
+//        for(int x : arr){
+//            map.put(x, map.getOrDefault(x, 0)+1);
+//        }
+//
+//        for(int x : map.keySet()){
+//            if(map.get(x) > 1) return 'D';
+//        }
 
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(arr[i] == arr[j]) return 'D';
-            }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n-1; i++){
+            if(arr[i] == arr[i+1]) return 'D';
         }
-
         return answer;
     }
     public static void main(String[] args) {
