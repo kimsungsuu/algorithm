62a90214cea93a8c8d28fb73db7b6334ec9d1da2
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-21 23:18:24 +0900

    study java algorithm

---
+++ src/main/java/infrenalgorithm/section06/five/Main.java
@@ -0,0 +1,31 @@
+package infrenalgorithm.section06.five;
+
+import java.util.Scanner;
+
+public class Main {
+    public char solution(int n, int[] arr){
+        char answer = 'U';
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] == arr[j]) return 'D';
+            }
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+
+        System.out.print(T.solution(n, arr));
+    }
+
+}
