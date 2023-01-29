8e00472d0c520d721c67bb8584985f773f7cee7e
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-29 13:43:44 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section06/nine/Main.java
+++ src/main/java/infrenalgorithm/section06/nine/Main.java
@@ -1,15 +1,40 @@
 package infrenalgorithm.section06.nine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    public int count(int[] arr, int capacity){
+        int cnt = 1, sum = 0;
+
+        for(int i : arr){
+            if(sum + i > capacity){
+                cnt++;
+                sum = i;
+            }
+            else sum += i;
+        }
+
+        return cnt;
+    }
+
     public int solution(int n, int m, int[] arr){
         int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
 
+        while(lt <= rt){
+            int mid = (lt+rt)/2;
+            if(count(arr, mid) <= m){
+                answer = mid;
+                rt = mid - 1;
+            }else lt = mid + 1;
+        }
         return answer;
     }
     public static void main(String[] args) {
-        My T = new My();
+        Main T = new Main();
         Scanner in = new Scanner(System.in);
 
         int n = in.nextInt();
