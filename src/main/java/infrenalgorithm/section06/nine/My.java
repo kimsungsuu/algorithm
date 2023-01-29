bf8d9ed4971d432ad72352e6cf2e7315ed362924
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-29 10:47:53 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section06/nine/My.java
+++ src/main/java/infrenalgorithm/section06/nine/My.java
@@ -12,8 +12,9 @@ public class My {
         int cnt = 0;
         int sum = arr[0];
 
-        for(int i = 0; i < n; i++){
-            int tmp = arr[i];
+        int tmp = arr[0];
+        for(int i = 0; i < n-1; i++){
+            tmp += arr[i];
 
             while(rt < n-1){
                 rt++;
@@ -26,13 +27,13 @@ public class My {
 
             }
 
-            System.out.println(cnt);
+
 
             if(cnt == m){
                 answer = tmp;
                 break;
             }
-
+            cnt = 0;
         }
 
 
