160c82012fea5c3e32ed4a8b0635325e060aee53
ksungsu <zxzxzlxlzlxl@naver.com>
2023-01-29 14:40:02 +0900

    study java algorithm

---
+++ src/main/java/programmers/Yesan.java
@@ -0,0 +1,39 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Yesan {
+        public int solution(int[] d, int budget) {
+            int answer = 0;
+
+            Arrays.sort(d);
+            int cnt = 0;
+
+            for(int i : d){
+                if(budget - i >= 0){
+                    budget -= i;
+                    cnt++;
+                }
+            }
+            answer = cnt;
+            return answer;
+        }
+        public static void main(String[] args){
+            Yesan T = new Yesan();
+            Scanner in = new Scanner(System.in);
+
+            int n = in.nextInt();
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++){
+                arr[i] = in.nextInt();
+            }
+
+            int budget = in.nextInt();
+
+            System.out.print(T.solution(arr, budget));
+
+        }
+    }
+
