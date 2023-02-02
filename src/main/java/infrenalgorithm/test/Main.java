62ba2ae2dfd4e77ce270a5b009be5bb801c00a4f
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-02 17:06:11 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/test/Main.java
+++ src/main/java/infrenalgorithm/test/Main.java
@@ -1,32 +1,50 @@
 package infrenalgorithm.test;
 
 
-import java.sql.SQLOutput;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-    public int solution(String skill){
-        int answer = 0;
+    public int[] solution(int n, String[] words) {
+        int[] answer = { 0, 0 };
+        char chEnd = words[0].charAt(words[0].length() - 1);
+        char chStart;
 
-        String tmp = "hello world";
+//        System.out.print(chEnd + " ");
+        HashSet<String> log = new HashSet<>();
+        log.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
 
-        System.out.println(skill.indexOf("w"));
-        System.out.println(tmp.indexOf("wor"));
-        System.out.println(skill.indexOf("wor"));
+            chStart = words[i].charAt(0);
+            log.add(words[i]);
+//            System.out.println("i = " + i + " " + " i%n+1 = " + (i%n+1));
 
+            if (chEnd != chStart || log.size() != i + 1) {
+//                System.out.print(i + 1 + "!");
+                answer[0] = (i % n)+1;
+                answer[1] = (i / n) + 1;
+                break;
+            }
 
+            chEnd = words[i].charAt(words[i].length() - 1);
+//            System.out.print(chEnd + " ");
+        }
+//        System.out.println();
+        System.out.println("(" + answer[0] + ", " + answer[1] + ")");
         return answer;
     }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
 
-        String skill = in.next();
+        int n = in.nextInt();
 
-        System.out.println(T.solution(skill));
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        System.out.println(T.solution(n, words));
+
 
     }
 }
