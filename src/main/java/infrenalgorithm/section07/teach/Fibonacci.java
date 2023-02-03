f6ac53d42358ad65d0d97a2f7c07cc9be470646a
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-03 18:34:12 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section07/teach/Fibonacci.java
+++ src/main/java/infrenalgorithm/section07/teach/Fibonacci.java
@@ -4,8 +4,7 @@ public class Fibonacci {
 
     public int fibonacci(int n){
 
-        if(n == 1) return 1;
-        else if(n == 2) return 1;
+        if(n == 1 || n == 2) return 1;
         else return fibonacci(n-1)+fibonacci(n-2);
     }
 
