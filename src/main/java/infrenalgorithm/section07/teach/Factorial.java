174c67083d050cf51026cc4e5649f9a59648940e
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-03 15:48:07 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section07/teach/Factorial.java
+++ src/main/java/infrenalgorithm/section07/teach/Factorial.java
@@ -2,19 +2,15 @@ package infrenalgorithm.section07.teach;
 
 public class Factorial {
     static int k = 1;
-    public void DFS(int n){
+    public int DFS(int n){
 
-        if(n==0) return;
-        else{
-            DFS(n-1);
-            k *= n;
-        }
+        if(n==1) return 1;
+        else return n*DFS(n-1);
     }
 
     public static void main(String[] args) {
         Factorial T = new Factorial();
 
-        T.DFS(5);
-        System.out.println(k);
+        System.out.println(T.DFS(5));
     }
 }
