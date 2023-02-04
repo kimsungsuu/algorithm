10dbb382947d60e33e670b666d30f9f80bfcfd5c
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-04 22:20:17 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/section07/teach/TreeTraversal.java
+++ src/main/java/infrenalgorithm/section07/teach/TreeTraversal.java
@@ -14,9 +14,16 @@ class Node{
 public class TreeTraversal {
     Node root;
 
+    public void DFS(Node root){
+        if(root == null) return; // DFS가 말단 노드까지 도착했다는 의미이므로 함수를 return;
+        else{
+            DFS(root.lt);
+            DFS(root.rt);
+            System.out.print(root.data + " ");
+        }
+    }
     public static void main(String[] args) {
         TreeTraversal tree = new TreeTraversal();
-
 
 
         tree.root = new Node(1);
@@ -26,5 +33,6 @@ public class TreeTraversal {
         tree.root.lt.rt = new Node(5);
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
+        tree.DFS(tree.root);
     }
 }
