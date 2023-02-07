a87198bef51b0edb300213a410667155d3d6298b
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-08 00:30:19 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/test/Main.java
+++ src/main/java/infrenalgorithm/test/Main.java
@@ -3,22 +3,28 @@ package infrenalgorithm.test;
 
 
 public class Main {
-    public int[] DFS(int n){
-
-        int[] answer = new int[n];
-
-        answer[0] = 1;
-        answer[1] = 1;
-
-        for(int i = 2; i < n; i++){
-            answer[i] = answer[i-1] + answer[i-2];
-        }
-
-        return answer;
-    }
+//    public int[] DFS(int n){
+//
+//        int[] answer = new int[n];
+//
+//        answer[0] = 1;
+//        answer[1] = 1;
+//
+//        for(int i = 2; i < n; i++){
+//            answer[i] = answer[i-1] + answer[i-2];
+//        }
+//
+//        return answer;
+//    }
     public static void main(String[] args) {
         Main T = new Main();
 
-        for(int x : T.DFS(10)) System.out.print(x + " ");
+        boolean[] visited = new boolean[6];
+
+        visited[1] = true;
+
+        for(int i = 2; i <= 5; i++){
+            System.out.println(!visited[i]);
+        }
     }
 }
