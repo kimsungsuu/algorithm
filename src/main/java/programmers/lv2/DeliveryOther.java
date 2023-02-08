5c3f4ff98a3ebb2377642939f988f870b8f60524
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-08 16:52:02 +0900

    study java algorithm

--- src/main/java/programmers/lv2/DeliveryOther.java
+++ src/main/java/programmers/lv2/DeliveryOther.java
@@ -30,6 +30,8 @@ public class DeliveryOther {
         }
 
         int[] dist = new int[N + 1];
+
+
         for (int i = 2; i <= N; i++) {
             dist[i] = (dist[i]==0) ? INF : map[1][i];
         }
@@ -37,22 +39,23 @@ public class DeliveryOther {
         boolean[] visited = new boolean[N + 1];
         visited[1] = true;
 
-        for (int i = 1; i <= N - 1; i++) { // n-1번 반복
+        for (int i = 1; i <= N - 1; i++) { // dist[2] ~ dist[N]까지 돌기 때문에 N-1번이다.
 
             // extract-min
-            // dist 중에 방문하지 않았고 가장 작은 값을 가지는 인덱스를 찾는다.
             int min_idx = 1;
             int min_value = INF;
             for (int j = 2; j <= N; j++) {
+                // 한번도 들리지 않았고(visited == false), dist[j] 리스트에서 가장 작은 인덱스일 때 조건문을 실행
                 if (!visited[j] && dist[j] < min_value) {
                     min_value = dist[j];
                     min_idx = j;
                 }
             }
 
+            //
             visited[min_idx] = true;
 
-            // 거쳐가는게 더 빠른지 확인
+            // 더 빠른 간선이 존재할 때 그 간선을 저장
             for (int j = 2; j <= N; j++) {
                 if (dist[j] > dist[min_idx] + map[min_idx][j]) {
                     dist[j] = dist[min_idx] + map[min_idx][j];
