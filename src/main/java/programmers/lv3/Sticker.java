b6d1931396d8bebb53b1c63a2eb02dbe38f470cf
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-09 22:45:23 +0900

    study java algorithm

--- src/main/java/programmers/lv3/Sticker.java
+++ src/main/java/programmers/lv3/Sticker.java
@@ -19,7 +19,6 @@ public class Sticker {
         dp1[1] = sticker[0];
         for (int i = 2; i < len-1;i++) {
             dp1[i] = Math.max(dp1[i-1],dp1[i-2] + sticker[i]);
-            System.out.println(dp1[i]);
         }
 
         System.out.println();
@@ -29,7 +28,6 @@ public class Sticker {
         dp2[1] = sticker[1];
         for (int i = 2; i < len; i++) {
             dp2[i] = Math.max(dp2[i-1],dp2[i-2] + sticker[i]);
-            System.out.println(dp2[i]);
         }
 
         answer = Math.max(dp1[len-2],dp2[len-1]);
