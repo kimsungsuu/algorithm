3a0adc7bb69a2cf426ca214e5fc705f7c4807111
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-09 21:12:59 +0900

    study java algorithm

---
+++ src/main/java/programmers/lv3/Sticker.java
@@ -0,0 +1,23 @@
+package programmers.lv3;
+
+import java.util.Scanner;
+
+public class Sticker {
+
+    public int solution(int sticker[]) {
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sticker T = new Sticker();
+        Scanner in = new Scanner(System.in);
+
+        int[] sticker = {14, 6, 5, 11, 3, 9, 2, 10};
+
+        System.out.println(T.solution(sticker));
+    }
+}
