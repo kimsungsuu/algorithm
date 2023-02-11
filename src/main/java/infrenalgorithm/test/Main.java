9dcbf0000452812109e397bf3d0d097e1b60375d
ksungsu <zxzxzlxlzlxl@naver.com>
2023-02-11 11:57:11 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/test/Main.java
+++ src/main/java/infrenalgorithm/test/Main.java
@@ -10,18 +10,51 @@ import java.util.Scanner;
 
 public class Main {
 
-    public int solution(int[] d, int budget) {
-        int answer = 0;
+    public int validation(int[] arr){
 
-        Arrays.sort(d);
+        String tmp1 = "";
 
-        int lt = 0;
+        int cnt = 1;
 
-        while(budget > 0){
-            budget -= d[lt];
-            lt++;
-            if(budget >= 0) answer++;
+        for(int i = 0; i < arr.length; i++){
+            tmp1 += arr[i];
         }
+
+        String tmp2 = new StringBuilder(tmp1).reverse().toString();
+
+        if(tmp1 == tmp2) return cnt;
+        else{
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] > 0) arr[i] -= 1;
+                break;
+            }
+
+
+        }
+
+        return cnt;
+    }
+
+    public int[] solution(int[][] queries) {
+        int[] answer = new int[2];
+
+        int n = queries.length;
+
+        int[] arr = new int[queries[0].length];
+
+
+        for(int i = 0; i < queries[0].length; i++){
+            for(int j = 0; j < queries[i].length; j++){
+                arr[i] = queries[i][j];
+                System.out.println(arr[i]);
+            }
+
+            if(validation(arr) == 1) answer[i] = 1;
+            else answer[i] = 0;
+
+        }
+
+        int lt = 0, rt = n-1;
 
 
         return answer;
@@ -31,15 +64,8 @@ public class Main {
             Main T = new Main();
             Scanner in = new Scanner(System.in);
 
-            int n = in.nextInt();
-            int[] arr = new int[n];
+            int[][] queries = {{2,0}, {3,1}};
 
-            for(int i = 0; i < n; i++){
-                arr[i] = in.nextInt();
-            }
-
-            int budget = in.nextInt();
-
-            System.out.print(T.solution(arr, budget));
+            System.out.println(T.solution(queries));
         }
     }
