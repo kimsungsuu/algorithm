4d48016b5ec59a026811c8fa1daeb31b6696be10
ksungsu <zxzxzlxlzlxl@naver.com>
2023-04-17 12:58:16 +0900

    study java algorithm

---
+++ src/main/java/infrenalgorithm/recursive_tree_graph_dfs_bfs/SixProblem.java
@@ -0,0 +1,40 @@
+package infrenalgorithm.recursive_tree_graph_dfs_bfs;
+
+import java.util.Scanner;
+
+public class SixProblem {
+
+    static int n;
+    static int[] ch;
+
+    public void D(int L){
+        if(L == n+1){
+
+            String tmp = "";
+            for(int i = 1; i <= n; i++){
+                if(ch[i] == 1) tmp += i + " ";
+            }
+            if(tmp.length() > 0) System.out.println(tmp);
+        }else{
+            ch[L] = 1;
+            D(L+1);
+            ch[L] = 0;
+            D(L+1);
+        }
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        SixProblem T = new SixProblem();
+        Scanner in = new Scanner(System.in);
+
+        n = 3;
+
+        ch = new int[n+1];
+
+        T.D(1);
+    }
+}
