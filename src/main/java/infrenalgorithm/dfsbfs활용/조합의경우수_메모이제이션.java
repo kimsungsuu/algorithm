9ab2fa0516731a69e6d01f5a87f5f91556a3922a
ksungsu <zxzxzlxlzlxl@naver.com>
2023-04-29 08:33:57 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/dfsbfs활용/조합의경우수_메모이제이션.java
+++ src/main/java/infrenalgorithm/dfsbfs활용/조합의경우수_메모이제이션.java
@@ -1,44 +1,15 @@
 package infrenalgorithm.dfsbfs활용;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class 조합의경우수_메모이제이션 {
-    static int n;
-    static int r;
-    static int[] arr;
-    static int[] result;
-    static int[] ch;
-    static int[][] fibo;
-    static int cnt;
 
+    int[][] fibo = new int[35][35];
 
-    public void DFS(int L){
-
-        if(L == r){
-            Arrays.sort(result);
-            for(int i = 0; i < cnt; i++){
-                if(!Arrays.equals(fibo[i], result)) return;
-            }
-
-            for(int j = 0; j < r; j++){
-                fibo[cnt][j] = result[j];
-                System.out.print(result[j] + " ");
-            }
-            System.out.println();
-            cnt++;
-
-        }else{
-            for(int i = 1; i <= n; i++){
-                if(ch[i] == 0){
-                    ch[i] = 1;
-                    result[L] = arr[i];
-                    DFS(L+1);
-                    ch[i] = 0;
-                }
-            }
-        }
+    public int DFS(int n, int r){
+        if(fibo[n][r] > 0) return fibo[n][r];
+        if(n==r || r==0) return 1;
+        else return fibo[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
     }
 
 
@@ -46,20 +17,9 @@ public class 조합의경우수_메모이제이션 {
         조합의경우수_메모이제이션 T = new 조합의경우수_메모이제이션();
         Scanner in = new Scanner(System.in);
 
-        n = in.nextInt();
-        r = in.nextInt();
-        cnt = 0;
+        int n = in.nextInt();
+        int r = in.nextInt();
 
-        ch = new int[n+1];
-        arr = new int[n+1];
-        fibo = new int[10000][r];
-
-        for(int i = 1; i <= n; i++){
-            arr[i] = i;
-        }
-
-        result = new int[r];
-
-        T.DFS(0);
+        System.out.println(T.DFS(n,r));
     }
 }
