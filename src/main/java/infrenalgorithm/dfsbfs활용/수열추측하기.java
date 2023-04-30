72b8beb854a6642b0734cd318848df21269fe524
ksungsu <zxzxzlxlzlxl@naver.com>
2023-04-30 10:04:26 +0900

    study java algorithm

---
+++ src/main/java/infrenalgorithm/dfsbfs활용/수열추측하기.java
@@ -0,0 +1,52 @@
+package infrenalgorithm.dfsbfs활용;
+
+import java.util.Scanner;
+
+public class 수열추측하기 {
+    int fibo[][] = new int[12][12];
+    static int[] p, b, ch;
+    static int n, total;
+
+    public int pDFS(int n, int k){
+        if(fibo[n][k] > 0) return fibo[n][k];
+        if(n == k || k == 0) return 1;
+        else return fibo[n][k] = pDFS(n-1,k-1) + pDFS(n-1, k);
+    }
+
+    public void bDFS(int L, int sum){
+        if(sum == total){
+            for(int i = 0; i < n; i++){
+                System.out.print(b[i] + " ");
+            }
+            System.out.println();
+        }
+        else{
+            for(int i = 1; i <= n; i++){
+                if(ch[i] == 0){
+                    ch[i] = 1;
+                    b[L] = i;
+                    bDFS(L+1,sum + p[i-1]*b[L]);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        수열추측하기 T = new 수열추측하기();
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+        total = in.nextInt();
+
+        p = new int[n];
+        b = new int[n];
+        ch = new int[n+1];
+
+        for(int k = 0; k < n; k++){
+            p[k] = T.pDFS(n-1, k);
+        }
+
+        T.bDFS(0,0);
+    }
+}
