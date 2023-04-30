69a6cb90fe347a1993d68e666a73217e7dbed7a2
ksungsu <zxzxzlxlzlxl@naver.com>
2023-05-01 07:43:28 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/dfsbfs활용/수열추측하기.java
+++ src/main/java/infrenalgorithm/dfsbfs활용/수열추측하기.java
@@ -6,26 +6,32 @@ public class 수열추측하기 {
     int fibo[][] = new int[12][12];
     static int[] p, b, ch;
     static int n, total;
+    boolean flag;
 
-    public int pDFS(int n, int k){
-        if(fibo[n][k] > 0) return fibo[n][k];
-        if(n == k || k == 0) return 1;
-        else return fibo[n][k] = pDFS(n-1,k-1) + pDFS(n-1, k);
+    public int pDFS(int n, int k) {
+        if (fibo[n][k] > 0) return fibo[n][k];
+        if (n == k || k == 0) return 1;
+        else return fibo[n][k] = pDFS(n - 1, k - 1) + pDFS(n - 1, k);
     }
 
-    public void bDFS(int L, int sum){
-        if(sum == total){
-            for(int i = 0; i < n; i++){
-                System.out.print(b[i] + " ");
+    public void bDFS(int L) {
+        if (flag) return;
+        if (L == n) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                sum += p[i] * b[i];
             }
-            System.out.println();
-        }
-        else{
-            for(int i = 1; i <= n; i++){
-                if(ch[i] == 0){
+            if (sum == total) {
+                for (int x : b) System.out.print(x + " ");
+                System.out.println();
+                flag = true;
+            }
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 0) {
                     ch[i] = 1;
                     b[L] = i;
-                    bDFS(L+1,sum + p[i-1]*b[L]);
+                    bDFS(L + 1);
                     ch[i] = 0;
                 }
             }
@@ -41,12 +47,12 @@ public class 수열추측하기 {
 
         p = new int[n];
         b = new int[n];
-        ch = new int[n+1];
+        ch = new int[n + 1];
 
-        for(int k = 0; k < n; k++){
-            p[k] = T.pDFS(n-1, k);
+        for (int k = 0; k < n; k++) {
+            p[k] = T.pDFS(n - 1, k);
         }
 
-        T.bDFS(0,0);
+        T.bDFS(0);
     }
 }
