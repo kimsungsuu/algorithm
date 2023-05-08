ca5a08c0a5e989775359338a4076011ea184319b
ksungsu <zxzxzlxlzlxl@naver.com>
2023-05-08 10:16:32 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/dfsbfs활용/피자배달거리.java
+++ src/main/java/infrenalgorithm/dfsbfs활용/피자배달거리.java
@@ -3,29 +3,67 @@ package infrenalgorithm.dfsbfs활용;
 import java.util.Scanner;
 
 public class 피자배달거리 {
-    static int n,m;
+    static int n, m;
     static int[][] arr;
+    static int cnt;
+    static int answer;
+
+    int[] dx = {-1, 0, 1, 0}; //상,우,하,좌
+    int[] dy = {0, 1, 0, -1}; //상,우,하,좌
+
+    static boolean flag;
+
+    static boolean[][] ch;
+
+    public void DFS(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            if (flag) return;
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx > 0 && ny > 0 && nx <= n && ny <= n && arr[nx][ny] == 2) {
+                answer++;
+                flag = true;
+                cnt++;
+                return;
+            }
+
+            if (nx > 0 && ny > 0 && nx <= n && ny <= n && arr[nx][ny] != 2 && !flag) {
+                if (!ch[nx][ny]) {
+                    ch[nx][ny] = true;
+                    DFS(nx, ny);
+                }
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
         피자배달거리 T = new 피자배달거리();
         Scanner in = new Scanner(System.in);
 
-        n = in.nextInt();;
+        n = in.nextInt();
         m = in.nextInt();
 
-        arr = new int[n+1][n+1];
+        arr = new int[n + 1][n + 1];
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 arr[i][j] = in.nextInt();
             }
         }
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                System.out.print(arr[i][j]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (arr[i][j] == 1 && cnt <= m) {
+                    flag = false;
+                    ch = new boolean[n + 1][n + 1];
+                    T.DFS(i, j);
+                }
             }
-            System.out.println();
         }
+
+        System.out.print(answer);
     }
 }
