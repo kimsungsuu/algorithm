86871847861c0bd1ec81418dd4b9c3155e7d1173
ksungsu <zxzxzlxlzlxl@naver.com>
2023-05-10 09:23:33 +0900

    study java algorithm

--- src/main/java/infrenalgorithm/dfsbfs활용/피자배달거리.java
+++ src/main/java/infrenalgorithm/dfsbfs활용/피자배달거리.java
@@ -6,26 +6,36 @@ public class 피자배달거리 {
     static int n, m;
     static int[][] arr;
     static int[] px, py;
-    static int[] chPx, chPy;
     static int cnt;
     static int[] resultX;
     static int[] resultY;
-    static int answer;
-
-    int[] dx = {-1, 0, 1, 0}; //상,우,하,좌
-    int[] dy = {0, 1, 0, -1}; //상,우,하,좌
-
-    static boolean flag;
-
-    static boolean[][] ch;
+    static int result;
+    static int temp3, temp4;
 
     // 피자집 갯수Cm으로 나올 수 있는 조합의 경우 들
     public void DFS(int L, int s) {
         if(L == n){
-            for (int i = 0; i < L; i++){
-                System.out.print("(" + resultX[i] + ", " + resultY[i] + "), ");
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    if(arr[i][j] == 1){
+                        int temp1;
+                        int temp2;
+                        temp3 = n;
+                        for(int k = 0; k < L; k++){
+                            if(i <= resultX[k]) temp1 = (resultX[k] - i);
+                            else temp1 = i - resultX[k];
+                            if(j <= resultY[k]) temp2 =(resultY[k] - j);
+                            else temp2 = j - resultY[k];
+
+                            temp3 = Math.min(temp3, temp1+temp2);
+                            if(temp3 == 1) break;
+                        }
+                        temp4 += temp3;
+                    }
+                }
             }
-            System.out.println();
+            result = Math.min(result, temp4);
+            temp4 = 0;
         }
         else{
             for(int i = s; i < cnt; i++){
@@ -45,12 +55,11 @@ public class 피자배달거리 {
 
         arr = new int[n + 1][n + 1];
 
+        // 모든 피자집 좌표를 저장할 배열 px, pu
         px = new int[n*n];
         py = new int[n*n];
 
-        chPx = new int[n*n];
-        chPy = new int[n*n];
-
+        // 모든 피자집에서 m개의 조합을 담을 배열
         resultX = new int[n];
         resultY = new int[n];
 
@@ -60,6 +69,7 @@ public class 피자배달거리 {
             }
         }
 
+        // 피자집 갯수만큼 px, py에 저장하기 위해 선언한 변수
         cnt = 1;
 
         // 값이 2인 좌표 저장하기
@@ -73,8 +83,9 @@ public class 피자배달거리 {
             }
         }
 
+        result = n*n;
         T.DFS(0,1);
 
-        System.out.print(answer);
+        System.out.print(result);
     }
 }
